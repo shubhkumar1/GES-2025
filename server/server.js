c43c760f3d5d2f78275376d059b6f5f36e11183d
Shubham Kumar <93635882+shubhkumar1@users.noreply.github.com>
2025-01-21 22:11:25 +0530

    Server update at 22:11

--- server/server.js
+++ server/server.js
@@ -260,7 +260,5 @@ app.post('/api/admin/login', async (req, res) => {
     }
 })
 
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
