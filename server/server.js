fe99736bd3e64612fb6500ef8507b36b342a8f28
Shubham Kumar <93635882+shubhkumar1@users.noreply.github.com>
2025-10-10 00:48:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://mcrcell.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 
 
